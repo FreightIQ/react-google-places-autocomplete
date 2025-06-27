8a9fe03765c6a091895351260297cee7c2f371e0
Shri Ram Kesava Raja <shriram.k.raja@gmail.com>
2025-06-27 15:36:00 +0530

    export new util

--- build/index.d.ts
+++ build/index.d.ts
@@ -3,5 +3,6 @@ import getLatLng from './utils/get-lat-lng';
 import geocodeByAddress from './utils/geocode-by-address';
 import geocodeByLatLng from './utils/geocode-by-lat-lng';
 import geocodeByPlaceId from './utils/geocode-by-place-id';
-export { getLatLng, geocodeByAddress, geocodeByLatLng, geocodeByPlaceId, };
+import getDetailsByPlaceId from './utils/get-details-by-place-id';
+export { getLatLng, geocodeByAddress, geocodeByLatLng, geocodeByPlaceId, getDetailsByPlaceId, };
 export default GooglePlacesAutocomplete;
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import getLatLng from './utils/get-lat-lng';
 import geocodeByAddress from './utils/geocode-by-address';
 import geocodeByLatLng from './utils/geocode-by-lat-lng';
 import geocodeByPlaceId from './utils/geocode-by-place-id';
+import getDetailsByPlaceId from './utils/get-details-by-place-id';
 
 export {
   getLatLng,
   geocodeByAddress,
   geocodeByLatLng,
   geocodeByPlaceId,
+  getDetailsByPlaceId,
 };
 export default GooglePlacesAutocomplete;
